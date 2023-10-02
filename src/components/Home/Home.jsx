3e9db07ef49f054db7d32473b49600fb38a88b0b
Dudkin Sergey <dsergik@mail.ru>
2023-10-02 15:25:18 +0300

    Адаптироавл   блоки  text и image, а также  добавил Swiper для блока logos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
+import uuid from 'react-uuid';
 import './home.scss';
 import image from '../../assets/img/Home/Header/image.png';
 import decor from '../../assets/img/Home/Header/Shapes.svg';
-import prev from '../../assets/icon/icon-prev.svg'
-import decorBtn from '../../assets/img/Shapes-btn.svg'
-
+import prev from '../../assets/icon/icon-prev.svg';
+import decorBtn from '../../assets/img/Shapes-btn.svg';
 
 import logo1 from '../../assets/img/Home/Header/Logo-1.svg';
 import logo2 from '../../assets/img/Home/Header/Logo-2.svg';
 import logo3 from '../../assets/img/Home/Header/Logo-3.svg';
 import logo4 from '../../assets/img/Home/Header/Logo-4.svg';
 import logo5 from '../../assets/img/Home/Header/Logo-5.svg';
+import Slider from './SliderLogos/Slider';
 
-
-
+function id() {
+  return uuid();
+}
 
 const Home = () => {
+  const logos = [
+    {
+      id: id(),
+      image: logo1,
+      title: 'Имя логотипа',
+    },
+    {
+      id: id(),
+      image: logo2,
+      title: 'Имя логотипа',
+    },
+    {
+      id: id(),
+      image: logo3,
+      title: 'Имя логотипа',
+    },
+    {
+      id: id(),
+      image: logo4,
+      title: 'Имя логотипа',
+    },
+    {
+      id: id(),
+      image: logo5,
+      title: 'Имя логотипа',
+    },
+  ];
+
   return (
     <div className="header">
       <div className="container">
@@ -28,20 +58,24 @@ const Home = () => {
               <h1 className="text__title">
                 Transform Your Idea Into Reality with Finsweet
               </h1>
-              <p className='text__info'>
+              <p className="text__info">
                 The entire Finsweet team knows what's good with Webflow and you
                 can too with 1 week and a good attitude.
               </p>
-              <button className='text__button'>
-                <img className='text__button--decor' src={decorBtn} alt="decor button" />
-                <span>Request Quote </span> 
-                <img className='text__button-icon'src={prev} alt="prev" /> 
-               </button>
+              <button className="text__button">
+                <img
+                  className="text__button--decor"
+                  src={decorBtn}
+                  alt="decor button"
+                />
+                <span>Request Quote </span>
+                <img className="text__button-icon" src={prev} alt="prev" />
+              </button>
             </div>
 
             <div className="image">
               <img className="image__left" src={decor} alt="Decor left" />
-              <img className='image-centr' src={image} alt="Image" />
+              <img className="image-centr" src={image} alt="Image" />
               <div className="image__right"></div>
             </div>
           </div>
@@ -51,12 +85,21 @@ const Home = () => {
               <p className="logos__paragrapg-small">Our Clients</p>
               <p className="logos__paragrapg-large">We've Worked with</p>
             </div>
-            <img className="logos__logo" src={logo1} alt="logo" />
-            <img className="logos__logo" src={logo2} alt="logo" />
-            <img className="logos__logo" src={logo3} alt="logo" />
-            <img className="logos__logo" src={logo4} alt="logo" />
-            <img className="logos__logo" src={logo5} alt="logo" />
+
+            {logos.map((logo) => {
+              return (
+                <img
+                  key={logo.id}
+                  className="logos__logo"
+                  src={logo.image}
+                  alt={logo.title}
+                />
+              );
+            })}
+
           </div>
+            <Slider logos={logos} />
+         
         </div>
       </div>
     </div>
---
+++ src/components/Home/SliderLogos/Slider.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+import './slider.scss';
+
+
+
+const Slider = ({ logos }) => {
+  return (
+    <div className="slider">
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={10}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+        }}
+      >
+        {logos.map((logo) => {
+          return (
+            <SwiperSlide>
+              <img src={logo.image} alt={logo.title} />
+            </SwiperSlide>
+          );
+        })}
+            <div className="swiper__controler none"></div>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
