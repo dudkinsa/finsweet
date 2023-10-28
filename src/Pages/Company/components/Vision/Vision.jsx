2f3e3e16cbf22965a056a844d77efb2578a60c77
Dudkin Sergey <dsergik@mail.ru>
2023-10-28 22:55:56 +0300

    Cоздал в странице Company компоент Vision

--- src/Pages/Company/components/Vision/Vision.jsx
+++ src/Pages/Company/components/Vision/Vision.jsx
@@ -7,16 +7,19 @@ const Vision = () => {
     <div className={styles.vision}>
       <div className="container">
         <div className={styles.vision__wrapper}>
-          <div className="caption">Our VISION</div>
-          <h3 className={styles.vision__title}>
-            We want to get local identification in every corner of the world in
-            this era of global citizenship.
-          </h3>
-          <p className={styles.vision__text}>
-            Through True Rich Attended does no end it his mother since real had
-            half every him case in packages enquire we up ecstatic unsatiable
-            saw his giving Remain expense you position concluded.
-          </p>
+          <div className={styles.vision__inner}>
+            <div className="caption">Our VISION</div>
+            <h3 className={styles.vision__title}>
+              We want to get local identification in every corner of the world
+              in this era of global citizenship.
+            </h3>
+            <p className={styles.vision__text}>
+              Through True Rich Attended does no end it his mother since real
+              had half every him case in packages enquire we up ecstatic
+              unsatiable saw his giving Remain expense you position concluded.
+            </p>
+          </div>
+
           <img src={wideImage} alt="Wide Image" />
         </div>
       </div>
