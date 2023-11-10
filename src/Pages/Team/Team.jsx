3f9d198ee07bd76e530e5208c7e892b698bc7f2c
Dudkin Sergey <dsergik@mail.ru>
2023-11-10 06:28:32 +0300

    Подключеник компонентов Blog и CTA к странице Team

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './team.module.scss';
 import Header from './components/Header/Header';
+import Blog from '../../components/Home/Home-components/Blog/Blog';
+import CTA from '../../components/CTA/CTA';
 
 const Team = () => {
   return (
     <>
        <Header />
+       <Blog />
+       <CTA />
     </>
   )
 }
